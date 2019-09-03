a6781d34307dab6fc15ebf6c23e0d65d782a8356
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-09-03 15:43:21 -0400

    Added getColor and getEmoji functions

--- src/Joke.js
+++ src/Joke.js
@@ -2,6 +2,42 @@ import React, { Component } from 'react'
 import "./Joke.css"
 
 export default class Joke extends Component {
+  getColor = () => {
+    if(this.props.score >= 15) {
+      return "#4CAF50";
+    } else if (this.props.score >= 12) {
+      return "#8BC34A";
+    } else if (this.props.score >= 9) {
+      return "#CDDC39";
+    } else if (this.props.score >= 6) {
+      return "#FFEB3B";
+    } else if (this.props.score >= 3) {
+      return "#FFC107";
+    } else if (this.props.score >= 0) {
+      return "#FF9800"
+    } else {
+      return "#f44336"
+    }
+  }
+
+  getEmoji = () => {
+    if (this.props.score >= 15) {
+      return "em em-rolling_on_the_floor_laughing";
+    } else if (this.props.score >= 12) {
+      return "em em-laughing";
+    } else if (this.props.score >= 9) {
+      return "em em-smiley";
+    } else if (this.props.score >= 6) {
+      return "em em-slightly_smiling_face";
+    } else if (this.props.score >= 3) {
+      return "em em-neutral_face";
+    } else if (this.props.score >= 0) {
+      return "em em-confused"
+    } else {
+      return "em em-angry"
+    }
+  }
+  
   upvote = () => {
     this.props.updateScore(this.props.id, this.props.score + 1);
   }
@@ -10,22 +46,19 @@ export default class Joke extends Component {
     this.props.updateScore(this.props.id, this.props.score - 1);
   }
 
-  // handleVoteUpdate = () => {
-  //   this.props.updateScore(this.props.id, this.state.score);
-  // }
-
   render() {
     const {joke} = this.props;
     return (
       <li className="Joke">
         <div className="Joke-score-div">
-          <button onClick={this.upvote} className="fas fa-arrow-up" />
-          <span>{this.props.score}</span>
-          <button onClick={this.downvote} className="fas fa-arrow-down" />
+          <i onClick={this.upvote} className="Joke-buttons fas fa-arrow-up" />
+          <span className="Joke-score" style={{borderColor: this.getColor()}}>{this.props.score}</span>
+          <i onClick={this.downvote} className="Joke-buttons fas fa-arrow-down" />
         </div>
-        <div className="Joke-string-div">
-        <p className="Joke-string">{joke}</p>
-        </div>
+        <div className="Joke-text">{joke}</div>
+        <div className="Joke-emoji" role="img" aria-label="rolling on floor laughing">
+          <i className={this.getEmoji()}></i>
+          </div>
       </li>
     )
   }
